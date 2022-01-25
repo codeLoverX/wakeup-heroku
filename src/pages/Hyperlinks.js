22d2ce57579e24c6c2049493e5da37921402448f
ridwanbinmonjur <ridwan@wavelet.net>
2022-01-25 20:28:29 +0600

    ammend

--- src/pages/Hyperlinks.js
+++ src/pages/Hyperlinks.js
@@ -1,22 +1,38 @@
 import urList from '../json/urlList.json'
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { MDBListGroup, MDBListGroupItem, MDBContainer } from "mdbreact";
 
 
 export let Hyperlinks = function () {
     return (
-        <div style={{ height: "100vh", width: "100vw", display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center"}}>
-        <div>Enter one of my fun projects by clicking on the links: </div>
-        {Object.keys(urList).map((value, index) => {
-            return (
-                <div>
-                    <Link to={`/${value}`}  >
-                        {value}
-                    </Link>
-                </div>
-            );
 
-        })}
-        </div>
-        )
+
+        
+
+            <MDBContainer>
+                <MDBListGroup style={{ width: "22rem", margin: "50px auto" }}>
+                    <MDBListGroupItem active>
+                        <div className="d-flex w-100 justify-content-between">
+                        </div>
+                        <p className="mb-1">Enter one of my fun projects by clicking on the links:</p>
+                    </MDBListGroupItem>
+                    {Object.keys(urList).map((value, index) => {
+                        return (
+
+                            <div>
+                                <Link to={`/${value}`}  >
+
+                                    <MDBListGroupItem>
+                                        {value}
+                                    </MDBListGroupItem>
+
+                                </Link>
+                            </div>
+                        );
+
+                    })}
+                </MDBListGroup>
+            </MDBContainer>
+    )
 }
